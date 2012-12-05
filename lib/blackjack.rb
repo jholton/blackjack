c4a27f83fbfb7c2d4a0dfdca2da5da900fb6541c
Johnny Holton <johnnyholton@gmail.com>
2012-12-04 23:32:07 -0500

    changes

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -7,6 +7,17 @@ require "blackjack/player"
 require "blackjack/blackjack_game"
 
 module Blackjack
+  module Input
+    class << self
+      def puts_and_say phrase
+        puts phrase
+        `say #{phrase}`
+      end
+    end
+  end
+
+  Object.extend Input
+
   puts "Would you like to play a game of Blackjack?"
   answer = gets.chomp
   if ["y","yes"].include? answer.downcase
@@ -17,13 +28,3 @@ module Blackjack
   end
 end
 
-module Input
-  class << self
-    def puts_and_say phrase
-      puts phrase
-      `say #{phrase}`
-    end
-  end
-end
-
-Object.extend Input
