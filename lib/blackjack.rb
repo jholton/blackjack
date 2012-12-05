540029708d8ecfd28940c1938ea596fda4ba06a1
Johnny Holton <johnnyholton@gmail.com>
2012-12-04 22:01:50 -0500

    changes

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -2,6 +2,8 @@ require "blackjack/version"
 require "blackjack/card"
 require "blackjack/deck"
 require "blackjack/participant"
+require "blackjack/dealer"
+require "blackjack/player"
 require "blackjack/blackjack_game"
 
 module Blackjack
